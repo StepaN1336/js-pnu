7d64ff36fa477739df5859471d89f1ddd2d8c6e5
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2025-06-02 01:10:38 +0000

    text translation, code formatting

--- team-proj/js/catalog.js
+++ team-proj/js/catalog.js
@@ -2,9 +2,9 @@ const categoriesContainer = document.querySelector('.catalog__categories');
 const productsContainer = document.querySelector('.catalog__products');
 
 fetch('json/catalog/categories.json')
-    .then(res => res.json())
-    .then(categories => {
-        categoriesContainer.innerHTML = categories.map(cat => `
+  .then(res => res.json())
+  .then(categories => {
+    categoriesContainer.innerHTML = categories.map(cat => `
       <div class="catalog__category" data-shortname="${cat.shortname}">
         <img src="${cat.image}" alt="${cat.name}" class="catalog__category-image">
         <h3 class="catalog__category-title">${cat.name}</h3>
@@ -12,24 +12,24 @@ fetch('json/catalog/categories.json')
       </div>
     `).join('');
 
-        document.querySelectorAll('.catalog__category').forEach(catEl => {
-            catEl.addEventListener('click', () => {
-                const shortname = catEl.dataset.shortname;
-                showProducts(shortname);
-            });
-        });
+    document.querySelectorAll('.catalog__category').forEach(catEl => {
+      catEl.addEventListener('click', () => {
+        const shortname = catEl.dataset.shortname;
+        showProducts(shortname);
+      });
     });
+  });
 
 function showProducts(shortname) {
-    categoriesContainer.classList.add('hidden');
-    productsContainer.classList.remove('hidden');
+  categoriesContainer.classList.add('hidden');
+  productsContainer.classList.remove('hidden');
 
-    productsContainer.innerHTML = `<button class="catalog__back-button">← Back</button>`;
+  productsContainer.innerHTML = `<button class="catalog__back-button">← Back</button>`;
 
-    fetch(`json/catalog/${shortname}.json`)
-        .then(res => res.json())
-        .then(products => {
-            productsContainer.innerHTML += `
+  fetch(`json/catalog/${shortname}.json`)
+    .then(res => res.json())
+    .then(products => {
+      productsContainer.innerHTML += `
         <div class="catalog__product-list">
           ${products.map(product => `
             <div class="catalog__product">
@@ -41,10 +41,10 @@ function showProducts(shortname) {
         </div>
       `;
 
-            document.querySelector('.catalog__back-button').addEventListener('click', () => {
-                productsContainer.classList.add('hidden');
-                categoriesContainer.classList.remove('hidden');
-                productsContainer.innerHTML = '';
-            });
-        });
+      document.querySelector('.catalog__back-button').addEventListener('click', () => {
+        productsContainer.classList.add('hidden');
+        categoriesContainer.classList.remove('hidden');
+        productsContainer.innerHTML = '';
+      });
+    });
 }
